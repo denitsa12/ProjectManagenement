8050971764a29ee6c984a555f2c765ac4f24fcfe
Denitsa <denitsaa.a.s@gmail.com>
2025-06-11 23:45:08 +0300

    Third commit/Added enum for the worker roles

--- 01.ProjectManagement/Models/Worker.cs
+++ 01.ProjectManagement/Models/Worker.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace _01.ProjectManagement.Models
 {
+    public enum Role
+    {
+        Leader,
+        Developer,
+        //QA,
+        //DevOps,
+        //SysAdmin,
+        //BusinessAnalyst
+    }
     internal class Worker
     {
         public string Name { get; set; }
         public int Salary { get; set; }
-        public string Position { get; set; }   
+        //public string Position { get; set; }   
+        public Role Role { get; set; }
     }
 }
--- 01.ProjectManagement/Services/Program.cs
+++ 01.ProjectManagement/Services/Program.cs
@@ -69,6 +69,11 @@ namespace _01.ProjectManagement.Services
 
             Console.WriteLine(" Въведи данни за Лидера:");
             Worker leader = ReadWorker();
+            while (leader.Role != Role.Leader)
+            {
+                Console.WriteLine("Грешка: лидерът трябва да бъде с роля 'Leader'. Моля, въведете отново.");
+                leader = ReadWorker();
+            }
 
             Console.Write("Описание на проекта: ");
             string description = Console.ReadLine();
@@ -105,7 +110,15 @@ namespace _01.ProjectManagement.Services
                 {
                     if(workers.Count < 5)
                     {
-                        workers.Add(ReadWorker());
+                        Worker dev = ReadWorker();
+                        if (dev.Role == Role.Leader)
+                        {
+                            Console.WriteLine("Грешка: само един лидер е позволен на проект. Избери друга роля.");
+                        }
+                        else
+                        {
+                            workers.Add(dev);
+                        }
                     }
                     else
                     {
@@ -130,14 +143,26 @@ namespace _01.ProjectManagement.Services
             Console.Write("Заплата: ");
             int salary = int.Parse(Console.ReadLine());
 
-            Console.Write("Позиция: ");
-            string position = Console.ReadLine();
+            //Console.Write("Позиция: ");
+            //string position = Console.ReadLine();
+
+            Console.WriteLine("Избери роля:");
+            foreach (var role in Enum.GetValues(typeof(Role)))
+            {
+                Console.WriteLine($"{(int)role} - {role}");
+            }
+
+            Console.Write("Избор (число): ");
+            int roleChoice = int.Parse(Console.ReadLine());
+
+            Role roleSelected = (Role)roleChoice;
 
             return new Worker
             {
                 Name = name,
                 Salary = salary,
-                Position = position
+                Role = roleSelected
+               // Position = position
             };
         }
 
@@ -156,7 +181,7 @@ namespace _01.ProjectManagement.Services
                 Console.WriteLine("Разработчици:");
                 foreach (var dev in p.Developers)
                 {
-                    Console.WriteLine($"  - {dev.Name}, {dev.Position}");
+                    Console.WriteLine($"  - {dev.Name}, {dev.Role}");
                 }
             }
         }
@@ -206,35 +231,35 @@ namespace _01.ProjectManagement.Services
             }
         }
 
-        static void SeedData()
-        {
-            Worker worker1 = new Worker { Name = "Петър", Position = "Младши програмист", Salary = 2000 };
-            Worker worker2 = new Worker { Name = "Георги", Position = "Старши програмист", Salary = 3000 };
-            Worker worker3 = new Worker { Name = "Виктор", Position = "QA", Salary = 4000 };
-            Worker worker4 = new Worker { Name = "Мария", Position = "DevOps", Salary = 5000 };
-            Worker worker5 = new Worker { Name = "Петя", Position = "Системен администратор", Salary = 6000 };
-            Worker worker6 = new Worker { Name = "Виктория", Position = "Старши програмист", Salary = 3000 };
-            Worker worker7 = new Worker { Name = "Димитър", Position = "QA", Salary = 4000 };
+        //static void SeedData()
+        //{
+        //    Worker worker1 = new Worker { Name = "Петър", Position = "Младши програмист", Salary = 2000 };
+        //    Worker worker2 = new Worker { Name = "Георги", Position = "Старши програмист", Salary = 3000 };
+        //    Worker worker3 = new Worker { Name = "Виктор", Position = "QA", Salary = 4000 };
+        //    Worker worker4 = new Worker { Name = "Мария", Position = "DevOps", Salary = 5000 };
+        //    Worker worker5 = new Worker { Name = "Петя", Position = "Системен администратор", Salary = 6000 };
+        //    Worker worker6 = new Worker { Name = "Виктория", Position = "Старши програмист", Salary = 3000 };
+        //    Worker worker7 = new Worker { Name = "Димитър", Position = "QA", Salary = 4000 };
 
-            projects.Add(new Project
-            {
-                Name = "Проект А",
-                StartDate = new DateTime(2023, 3, 1),
-                EndDate = new DateTime(2023, 9, 1),
-                Leader = worker2,
-                Description = "Модул за управление на данни",
-                Developers = new List<Worker> { worker1, worker3, worker4 }
-            });
+        //    projects.Add(new Project
+        //    {
+        //        Name = "Проект А",
+        //        StartDate = new DateTime(2023, 3, 1),
+        //        EndDate = new DateTime(2023, 9, 1),
+        //        Leader = worker2,
+        //        Description = "Модул за управление на данни",
+        //        Developers = new List<Worker> { worker1, worker3, worker4 }
+        //    });
 
-            projects.Add(new Project
-            {
-                Name = "Проект Б",
-                StartDate = new DateTime(2023, 1, 15),
-                EndDate = new DateTime(2023, 12, 1),
-                Leader = worker6,
-                Description = "Финансова система",
-                Developers = new List<Worker> { worker5, worker7 }
-            });
-        }
+        //    projects.Add(new Project
+        //    {
+        //        Name = "Проект Б",
+        //        StartDate = new DateTime(2023, 1, 15),
+        //        EndDate = new DateTime(2023, 12, 1),
+        //        Leader = worker6,
+        //        Description = "Финансова система",
+        //        Developers = new List<Worker> { worker5, worker7 }
+        //    });
+        //}
     }
 }
